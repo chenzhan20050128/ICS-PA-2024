46aacd48406d6ad3b3011b92b01777b11f4a4abe
tracer-ics2024 <tracer@njuics.org>
2024-10-26 20:31:29 +0800

    >  compile NEMU 231098166 ChenZhan Linux chenzhan-VMware-Virtual-Platform 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:31:29 up  4:26,  1 user,  load average: 1.92, 1.67, 1.55

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,6 +5,45 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
+// 保存当前分配内存的起始位置
+static uintptr_t addr = 0;
+
+// 初始化堆起始地址 add by cz at 1026 20:31
+void init_heap()
+{
+  addr = (uintptr_t)heap.start;
+}
+
+// 实现简单的 malloc() 函数
+void *malloc(size_t size)
+{
+  // 确保 malloc 不在初始化期间被调用，避免递归错误
+#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+  if (addr == 0)
+  {
+    panic("Heap not initialized");
+  }
+
+  // 将请求的内存大小对齐到 8 字节（或其他对齐要求）
+  size = ROUNDUP(size, 8);
+
+  // 检查是否有足够的空间
+  if (addr + size > (uintptr_t)heap.end)
+  {
+    panic("Out of memory");
+    return NULL;
+  }
+
+  // 保存当前地址作为返回值，并更新 addr
+  void *allocated = (void *)addr;
+  addr += size;
+
+  return allocated;
+#else
+  return NULL;
+#endif
+}
+
 int rand(void)
 {
   // RAND_MAX assumed to be 32767
@@ -35,17 +74,6 @@ int atoi(const char *nptr)
     nptr++;
   }
   return x;
-}
-
-void *malloc(size_t size)
-{
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  // panic("Not implemented");
-#endif
-  return NULL;
 }
 
 void free(void *ptr)
