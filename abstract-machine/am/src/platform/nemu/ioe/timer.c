0abf2147755d5818b4a5bc7a66cd717da590c3f8
tracer-ics2024 <tracer@njuics.org>
2024-10-26 17:44:09 +0800

    >  compile NEMU 231098166 ChenZhan Linux chenzhan-VMware-Virtual-Platform 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:44:09 up  1:39,  1 user,  load average: 0.27, 0.33, 0.40

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,18 +1,24 @@
 #include <am.h>
 #include <nemu.h>
 
-void __am_timer_init() {
+void __am_timer_init()
+{
 }
 
-void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
+{
+  // 读取32位的RTC寄存器来获取当前系统时间的微秒数
+  uint32_t lo = inl(RTC_ADDR);     // 低32位
+  uint32_t hi = inl(RTC_ADDR + 4); // 高32位
+  uptime->us = ((uint64_t)hi << 32) | lo;
 }
 
-void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
+void __am_timer_rtc(AM_TIMER_RTC_T *rtc)
+{
   rtc->second = 0;
   rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
+  rtc->hour = 0;
+  rtc->day = 0;
+  rtc->month = 0;
+  rtc->year = 1900;
 }
